0f85bd5bd7ca0086f051d527617499a16ffc047a
Melnychenko Oleksandr <melnichenkoalexandr1@gmail.com>
2023-07-31 13:52:22 +0300

    4 commit
    Переписал скрипт камеры и немного изменил расположение игрока

--- The Coin Dash/Assets/Scripts/CameraController.cs
+++ The Coin Dash/Assets/Scripts/CameraController.cs
@@ -1,21 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-    private Vector3 offset;
+    public Transform _player;
+    public Vector3 offset;
+    public float smoothSpeed = 5f;
 
-    void Start()
+    void FixedUpdate()
     {
-        offset = transform.position - player.position;
+            FollowPlayer();
+            Offset();
     }
 
-
-    void Update()
+    void FollowPlayer()
     {
-        Vector3 newPosition = new Vector3(player.position.x, transform.position.y, offset.z + player.position.z);
-        transform.position = newPosition;
+        Vector3 targetPosition = new Vector3(0, _player.position.y, _player.position.z) + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, _player.position.z + offset.z);
+        transform.rotation = Quaternion.Euler(14.5f, 0, 0);
+    }
+    private void Offset()
+    {
+        if (_player.transform.position.x > 1.7)
+        {
+            offset.x = 5.5f;
+            float newX = Mathf.Lerp(transform.position.x, offset.x, Time.fixedDeltaTime * smoothSpeed);
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+        if (_player.transform.position.x > -1.7 && _player.transform.position.x < 1.7)
+        {
+            offset.x = 0;
+            float newX = Mathf.Lerp(transform.position.x, offset.x, Time.fixedDeltaTime * smoothSpeed);
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+        if (_player.transform.position.x < -1.7)
+        {
+            offset.x = -5.5f;
+            float newX = Mathf.Lerp(transform.position.x, offset.x, Time.fixedDeltaTime * smoothSpeed);
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
     }
 }
